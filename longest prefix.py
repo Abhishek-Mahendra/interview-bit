bffa0053367e5e82508795cc2167f158f7ce658d
Abhishek <66868934+Abhishek-Mahendra@users.noreply.github.com>
2021-08-14 22:50:44 +0530

    Update longest prefix.py

--- longest prefix.py
+++ longest prefix.py
@@ -1,1 +1,13 @@
-
+class Solution:
+	# @param A : list of strings
+	# @return a strings
+	def longestCommonPrefix(self, A):
+		l=min(A,key=len)
+		count=0
+		j=0
+		for i in l:
+			for j in range(len(A)):
+				if A[j][count]!=i:
+					return l[:count]
+			count+=1
+		return l
